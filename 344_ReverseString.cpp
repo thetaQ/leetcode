7076653f6638568ddd9a87b2723ca4815ecfde42
tangqi <tangqi@iie.ac.cn>
2016-05-18 21:22:23 +0800

    344 Reverse String, Accept

---
+++ 344_ReverseString.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+    public:
+    string reverseString(string s) {
+        auto start = s.begin(), end = s.end() - 1;
+        while(start < end)
+        {
+            //swap
+            auto tmp = *start;
+            *start = *end;
+            *end = tmp;
+
+            start ++;
+            end --;
+        }
+        return s; 
+    }
+};
+
+
+int main(void)
+{
+    Solution solution; 
+    cout << solution.reverseString("hello") << endl;
+    return 0;
+}
