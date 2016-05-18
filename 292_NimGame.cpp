c3013da9a072a4f01e0b7e00f2806308bedbb715
tangqi <tangqi@iie.ac.cn>
2016-05-18 21:37:07 +0800

    292 Nim Game, Accepted

---
+++ 292_NimGame.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+
+    public:
+    bool canWinNim(int n) {
+        return n%4 != 0; 
+    }
+
+};
+
+
+int main(void)
+{
+    Solution solution;
+    cout << solution.canWinNim(10) << endl;
+}
+        
+
