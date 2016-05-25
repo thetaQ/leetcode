444e5318a015e7dc8edabe2268d861f8dd5b12f8
tangqi <tangqi@iie.ac.cn>
2016-05-25 22:03:31 +0800

    解决冲突

--- 021_MergeTwoSortedLists.cpp
+++ 021_MergeTwoSortedLists.cpp
@@ -9,11 +9,7 @@ struct ListNode {
 class Solution {
 public:
 
-<<<<<<< HEAD
     //Solution one: iterative
-=======
-    //Solution one: recursive
->>>>>>> origin/master
     ListNode * mergeTwoLists(ListNode *l1, ListNode *l2) 
     {
         if(l1 == NULL)
