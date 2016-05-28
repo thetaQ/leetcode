c7ba97458807a7466bad0dec1bea9f925742abb2
tangqi <tangqi@iie.ac.cn>
2016-05-28 14:52:11 +0800

    026 Remove Duplicates From Sorted Array, Accepted

---
+++ 026._RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        int size = nums.size();
+        if(size == 0)
+            return 0;
+        int i = 0;
+        for(int j = 1; j < size; j++)
+        {
+            if(nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
