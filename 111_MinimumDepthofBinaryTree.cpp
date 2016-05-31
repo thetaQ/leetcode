0fa846560296100cbb9fe6bf67c6a06dd9f423f5
tangqi <tangqi@iie.ac.cn>
2016-05-31 21:58:29 +0800

    111 MInimum Depth Of Binary Tree, Accepted

---
+++ 111_MinimumDepthofBinaryTree.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+ 
+class Solution {
+public:
+    int minDepth(TreeNode *root)
+    {
+        if(root == NULL)
+            return 0;
+        if(root->left == NULL && root->right != NULL)
+        {
+            return 1 + minDepth(root->right);
+        }
+        else if(root->left != NULL && root->right == NULL)
+        {
+            return 1 + minDepth(root->left);
+        }
+        else
+        {
+            return 1 + min(minDepth(root->left), minDepth(root->right));
+        }
+    }
+};
