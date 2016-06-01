ff9df0a294401c99a7fd81cacab6d6b81f7fcfb8
tangqi <tangqi@iie.ac.cn>
2016-06-01 12:24:28 +0800

    075 Sort Colors, Accepted

---
+++ 075_SortColors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    
+    void swap(vector<int>& nums, int a, int b)
+    {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+
+    void sortColors(vector<int>& nums) 
+    {
+        int l = 0, r = nums.size() - 1;
+        for(int index = 0; index < nums.size(); index ++)
+        {
+            if(nums[index] == 0)
+            {
+                swap(nums, index, l++);
+            }
+            else if(nums[index] == 2)
+            {
+                swap(nums, index--, r--);
+            }
+        }
+    }
+};
