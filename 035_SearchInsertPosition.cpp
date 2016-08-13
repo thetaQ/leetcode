d2e6c4a53a75416201e2adbde4224ac4324f6ddb
tangqi <tangqi@iie.ac.cn>
2016-08-13 20:32:12 +0800

    035 Search Insert Position, Accepted

---
+++ 035_SearchInsertPosition.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int start = 0, end = nums.size() - 1;
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return start;
+    }
+};
