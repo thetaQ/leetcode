ff7e3e94be4ecad6d1dd6d413d6f33474be34dca
tangqi <tangqi@iie.ac.cn>
2016-08-22 23:42:23 +0800

    069 Sqrt(x) Binary Search, Accepted

--- 069_Sqrt(x).cpp
+++ 069_Sqrt(x).cpp
@@ -12,4 +12,20 @@ public:
             v = (v + x / v) >> 1;
         return v;
     }
+
+    int mySqrt(int x)
+    {
+        if(x <= 1)
+            return x;
+        int start = 1, end = x;
+        while(start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if(mid <= x/mid)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return start - 1;
+    }
 };
