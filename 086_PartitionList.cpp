11a9db767ac05a592d07d5c7d84207a99c54196a
tangqi <tangqi@iie.ac.cn>
2016-08-24 21:34:46 +0800

    086 Partition List, Accepted

---
+++ 086_PartitionList.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *partition(ListNode *head, int x) {
+        // write your code here
+        ListNode node1(0), node2(0);
+        ListNode *p1 = &node1;
+        ListNode *p2 = &node2;
+        while(head != NULL)
+        {
+            if(head->val < x)
+            {
+                p1->next = head;
+                p1 = p1->next;
+            }
+            else
+            {
+                p2->next = head;
+                p2 = p2->next;
+            }
+            head = head->next;
+        }
+        p2->next = NULL;
+        p1->next = node2.next;
+        return node1.next;
+    }
+};
