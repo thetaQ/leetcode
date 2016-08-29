b8ccd634d02015f4f4c04cedc8b8841efb896e60
tangqi <tangqi@iie.ac.cn>
2016-08-29 15:15:09 +0800

    044 Wildcard Matching, Add DP Solution

--- 044_WildcardMatching.cpp
+++ 044_WildcardMatching.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    /**
-     * @param s: A string 
-     * @param p: A string includes "?" and "*"
-     * @return: A boolean
-     */
+    
+    //solution one: O(n)
     bool isMatch(const char *s, const char *p) {
         if(s == NULL || p == NULL)
             return false;
@@ -42,5 +41,37 @@ public:
         while(*p == '*')
             p++;
         return *p == '\0';
+    }
+
+
+    //solution two： DP
+    bool isMatch(string s, string p)
+    {
+        vector<vector<bool> > dp(s.size()+1, vector<bool>(p.size()+1, false));
+        dp[0][0] = true;
+
+        for(int j = 1; j <= p.size(); j++)
+        {
+            if(p[j-1] == '*')
+                dp[0][j] = true;
+            else
+                break;
+        }
+
+        for(int i = 1; i <= s.size(); i++)
+        {
+            for(int j = 1; j <= p.size(); j++)
+            {
+                if(p[j-1] == '*')
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '?');
+                }
+            }
+        }
+        return dp[s.size()][p.size()];
     }
 };
