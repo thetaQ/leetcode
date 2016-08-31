96b6f2344cc801c540c1934a46803de69ebd524d
tangqi <tangqi@iie.ac.cn>
2016-08-31 14:54:04 +0800

    322 Coin Change, Accepted

---
+++ 322_CoinChange.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++)
+        {
+            for(int j = 0; j < coins.size(); j++)
+            {
+                if(i >= coins[j])
+                    dp[i] = min(dp[i], dp[i-coins[j]] + 1); //选当前的或者不选当前的
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
